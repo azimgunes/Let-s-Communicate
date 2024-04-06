fe4df4cea6b71f0b5720117f7cba8c7ea05f1d6b
azimgunes <azimgunesofficial@gmail.com>
2024-04-06 21:40:49 +0300

    RegisterVC added.

---
+++ Let's Communicate/RegisterVC.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterVC.swift
+//  Let's Communicate
+//
+//  Created by Azim Güneş on 6.04.2024.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    //MARK: Proporties
+    
+    //MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+        
+    }
+}
+
+
+//MARK: Helpers
+
+
+extension RegisterVC {
+    private func style(){
+        view.backgroundColor = .white
+    }
+    
+    private func layout(){
+        
+    }
+}
