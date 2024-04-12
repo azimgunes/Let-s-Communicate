6dba392412b1863a506c0cd7d9a79c5e2feeff54
azimgunes <azimgunesofficial@gmail.com>
2024-04-12 21:57:41 +0300

    RegisterVC updated. Button added for selection.

--- Let's Communicate/RegisterVC.swift
+++ Let's Communicate/RegisterVC.swift
@@ -2,34 +2,7 @@
 //  RegisterVC.swift
 //  Let's Communicate
 //
-//  Created by Azim Güneş on 6.04.2024.
+//  Created by Azim Güneş on 7.04.2024.
 //
 
-import UIKit
-
-class RegisterVC: UIViewController {
-    //MARK: Proporties
-    
-    //MARK: Lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        style()
-        layout()
-        
-    }
-}
-
-
-//MARK: Helpers
-
-
-extension RegisterVC {
-    private func style(){
-        view.backgroundColor = .white
-    }
-    
-    private func layout(){
-        
-    }
-}
+import Foundation
