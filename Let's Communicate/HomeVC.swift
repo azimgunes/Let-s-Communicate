c60418840598d5a5698bc04523246a2452f07fde
azimgunes <azimgunesofficial@gmail.com>
2024-05-06 14:39:57 +0300

    HomeVC updated. NabBarItems created.

--- Let's Communicate/HomeVC.swift
+++ Let's Communicate/HomeVC.swift
@@ -16,6 +16,8 @@ class HomeVC: UIViewController {
     private var chatsButton: UIBarButtonItem!
     private var messageScreen: UIBarButtonItem!
     
+    private var container = Container()
+    
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,12 +35,49 @@ class HomeVC: UIViewController {
 
 extension HomeVC {
     
+    private func configureBar(text: String, selector: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(text, for: .normal)
+        button.setTitleColor(UIColor.purple, for: .normal)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return button
+    }
+    
     private func style(){
         configureGradient()
+        chatsButton = UIBarButtonItem(customView: configureBar(text: "Chats", selector: #selector(chatButton)))
+        messageScreen = UIBarButtonItem(customView: configureBar(text: "Message", selector: #selector(messageButton)))
+        
+        
+        //self.navigationItem.leftBarButtonItems = [chatsButton, messageScreen]
+        
+        self.navigationItem.leftBarButtonItems = [chatsButton]
+        
+        self.navigationItem.rightBarButtonItems = [messageScreen]
+        
+        //Container
+        
+        configureContainer()
 
     }
     
     private func layout(){
+        
+    }
+    
+    private func configureContainer(){
+        guard let containerView = container.view else {return}
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+    
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
         
     }
     
@@ -60,5 +99,28 @@ extension HomeVC {
     private func signOut(){
         try? Auth.auth().signOut()
         AuthStatus()
+    }
+}
+
+
+//MARK: Selectors
+
+
+extension HomeVC{
+    
+    @objc private func chatButton(){
+        
+    }
+    
+    @objc private func messageButton(){
+        
+    }
+}
+
+
+class Container: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureGradient()
     }
 }
