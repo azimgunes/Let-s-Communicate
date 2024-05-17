5de3fb512538b476c795431400dc2ae3e3bdf3c7
azimgunes <azimgunesofficial@gmail.com>
2024-05-17 12:59:05 +0300

    tableView updated.

--- Let's Communicate/MessageVC.swift
+++ Let's Communicate/MessageVC.swift
@@ -13,6 +13,8 @@ private let reuseId = "userCell"
 
 class MessageVC: UIViewController {
     
+    
+    
     //MARK: Properties
     private let tableView = UITableView()
     
@@ -22,7 +24,7 @@ class MessageVC: UIViewController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor =  #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        view.backgroundColor = .white
         style()
         layout()
     
@@ -38,20 +40,25 @@ extension MessageVC{
         tableView.delegate =  self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseId)
-        tableView.rowHeight = 75
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .clear
-        tableView.estimatedRowHeight = 30
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 30    
+    
         
     }
     private func layout(){
+        
+
         view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            
             
         ])
     }
@@ -62,12 +69,17 @@ extension MessageVC: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseId, for: indexPath)
         cell.backgroundColor = .white
-        cell.layer.cornerRadius = 15
-        cell.layer.borderColor = UIColor.clear.cgColor
+        cell.layer.cornerRadius = 10
+        cell.layer.borderWidth = 2
+        cell.layer.borderColor = UIColor(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)).cgColor
+        cell.selectionStyle = .none
+
         return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
+
+        
 }
