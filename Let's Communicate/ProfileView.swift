a98e7577f97c3610c7337966663a17e33d73f3ed
azimgunes <azimgunesofficial@gmail.com>
2024-06-17 15:47:47 +0300

    profileView updated.

--- Let's Communicate/ProfileView.swift
+++ Let's Communicate/ProfileView.swift
@@ -11,6 +11,43 @@ import FirebaseAuth
 class ProfileView: UIView {
     
     //MARK: Proporties
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor =  #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textColor = .white
+        label.text = "Azim Güneş"
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textColor = .white
+        label.text = "malatyalii"
+        return label
+    }()
+    
+    private let signoutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Out", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .red
+        return button
+        
+        
+    }()
+    
+    private lazy var stackView = UIStackView()
+
     //MARK: Lifecycle
     
     
@@ -23,7 +60,10 @@ class ProfileView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+    }
     
 }
 
@@ -32,9 +72,34 @@ class ProfileView: UIView {
 
 extension ProfileView{
     private func style(){
-        backgroundColor =  #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        backgroundColor = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 0.3096261161)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.layer.cornerRadius = 60
+        
+        stackView = UIStackView(arrangedSubviews: [nameLabel, usernameLabel, signoutButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        stackView.spacing = 5
+        
+       
     }
     private func layout(){
+        addSubview(profileImageView)
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: topAnchor, constant: 120),
+            profileImageView.widthAnchor.constraint(equalToConstant: 120),
+            profileImageView.heightAnchor.constraint(equalToConstant: 120),
+            profileImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            
+            stackView.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 12)
+        ])
         
     }
     private func fetchUser(){
